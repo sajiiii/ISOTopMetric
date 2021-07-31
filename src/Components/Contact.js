98b55b849fb3128144f3c1fe17466c8262629df3
sajid.s@caratlane.com <sajid.s@caratlane.com>
2021-07-31 15:08:29 +0530

    updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -140,7 +140,7 @@ class Contact extends Component {
                   {street} <br />
                   {city}, {state} {zip}
                   <br />
-                  <span>{phone}</span>
+                  {/* <span>{phone}</span> */}
                 </p>
               </div>
             </aside>
